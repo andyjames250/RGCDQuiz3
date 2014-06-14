beef925870cf44773b3d6eb2bac6c7a1f501f746
andyjames250 <andyjames250@gmail.com>
2014-06-14 23:17:01 +1000

    Updated Q1

--- Question1.R
+++ Question1.R
@@ -1,3 +1,4 @@
 download.file(url="https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv ", destfile="data.csv", method="curl")
 data <- read.csv("data.csv")
 agriculturalLogical <- data$ACR==3 & data$AGS==6
+head(which(agriculturalLogical==TRUE))
